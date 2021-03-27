6e8523def0a9caee00ba6a8968c79634b42c1c97
Jeff <abc@abc.com>
2021-03-27 10:58:12 -0400

    Indicate selected feed.

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -11,7 +11,6 @@ class AppState extends ChangeNotifier{
   // when the app starts.
   AppState() {
     selectedFeed = 0;             // Indicate no feed selected.
-    selectedFeedItem;             // Selected feed item
   }
 
   void selectFeed(int feedId) {
--- lib/widgets/feed_list_widget.dart
+++ lib/widgets/feed_list_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:rss_reader_plus/models/app_state.dart';
 import 'package:rss_reader_plus/models/feed.dart';
@@ -69,20 +70,44 @@ class _FeedListWidgetState extends State<FeedListWidget> {
   }
 
   Widget _buildFeedRow(BuildContext context, Feed feed, AppState appState) {
-    return Padding(
+    return Container(
       padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-      child: GestureDetector(
-        onTap: () async {
-          print("Tapped on feed ${feed.name}");
-          _previousScrollPosition = _controller.position.pixels;
-          appState.selectFeed(feed.id);
-        },
-        child: Row(
-          children: <Widget>[
-            Flexible(child: Text(feed.name, overflow: TextOverflow.ellipsis)),
-          ],
+      color: _backgroundColor(feed, appState),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: GestureDetector(
+          onTap: () async {
+            print("Tapped on feed ${feed.name}");
+            _previousScrollPosition = _controller.position.pixels;
+            appState.selectFeed(feed.id);
+          },
+          child: Row(
+            children: <Widget>[
+              Expanded(child: _feedRowText(context, feed, appState)),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  bool _isSelected(Feed feed, AppState appState) {
+    return feed.id == appState.selectedFeed;
+  }
+
+  Color _backgroundColor(Feed feed, AppState appState) {
+    // TODO: Use theme colors here
+    return _isSelected(feed, appState) ? Colors.blue : Colors.white;
+  }
+
+  Color _textColor(Feed feed, AppState appState) {
+    // TODO: Use theme colors here
+    return _isSelected(feed, appState) ? Colors.white : Colors.black;
+  }
+
+  Widget _feedRowText(BuildContext context, Feed feed, AppState appState) {
+    return Text(feed.name, overflow: TextOverflow.ellipsis,
+      style: TextStyle(color: _textColor(feed, appState)),
     );
   }
 }
