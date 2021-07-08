d09e9b907776732dfe7c4408fa1c41a7021dc3ab
Jeff <abc@abc.com>
2021-07-08 18:43:34 -0400

    Remove incorrect importing of cupertino.dart.

--- lib/services/initialization_service.dart
+++ lib/services/initialization_service.dart
@@ -1,5 +1,5 @@
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 import 'package:rss_reader_plus/services/feed_database.dart';
