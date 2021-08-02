5f192bc1d167f11724fe6e315cd7fba85a6645cd
Jeff <abc@abc.com>
2021-08-01 21:07:23 -0400

    Beginnings of code to support network proxies.  Unfortunately, proxy support is not too robust right now, so I'm shelving it for the time being.

--- lib/services/network_service.dart
+++ lib/services/network_service.dart
@@ -1,11 +1,43 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
 
 class NetworkService {
 
+  static IOClient createProxyAwareHttpClient() {
+    HttpClient httpClient = new HttpClient();
+
+    // From https://flutterigniter.com/debugging-network-requests/
+
+    // Make sure to replace <YOUR_LOCAL_IP> with 
+    // the external IP of your computer if you're using Android. 
+    // Note that we're using port 8888 which is Charles' default.
+    String proxy = Platform.isAndroid ? '<YOUR_LOCAL_IP>:8888' : 'localhost:8888';
+
+    // Hook into the findProxy callback to set
+    // the client's proxy.
+    httpClient.findProxy = (uri) {
+      return "PROXY $proxy;";
+    };
+
+    // This is a workaround to allow Charles to receive
+    // SSL payloads when your app is running on Android.
+    httpClient.badCertificateCallback = 
+      ((X509Certificate cert, String host, int port) => Platform.isAndroid);
+
+    // Pass your newly instantiated HttpClient to http.IOClient.
+    IOClient myClient = IOClient(httpClient);
+
+    return myClient;
+  }
+
   static Future<String> getFeed(String url) async {
+    // final client = createProxyAwareHttpClient();
+
     final uri = Uri.parse(url);
+    // final response = await client.get(uri);
     final response = await http.get(uri);
 
     if (response.statusCode == 200) {
@@ -16,7 +48,10 @@ class NetworkService {
   }
 
   static Future<Uint8List> getIcon(String url) async {
+    // final client = createProxyAwareHttpClient();
+
     final uri = Uri.parse(url);
+    // final response = await client.get(uri);
     final response = await http.get(uri);
 
     if (response.statusCode == 200) {
